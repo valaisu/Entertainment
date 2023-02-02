32c452433c281049eb58f02768ef3a8c9b3d4451
valaisu <63551725+valaisu@users.noreply.github.com>
2023-02-02 15:37:24 +0200

    Update drawExperimanes.py
    fix bugs, made style more functional, add functions

--- 3DRubicsCube/drawExperimanes.py
+++ 3DRubicsCube/drawExperimanes.py
@@ -14,16 +14,11 @@ import copy
 #      -Add more cubes, make them rotatable around x y and z axis
 #      -Add keybinds for each rubics cube move
 
-
+# Define some functions
 
 def inversion2D(coordList, height):
     for i in range(len(coordList)):
         coordList[i] = [coordList[i][0], height-coordList[i][1]]
-    return coordList
-
-def inversion3D(coordList, height):
-    for i in range(len(coordList)):
-        coordList[i] = [coordList[i][0], height-coordList[i][1], coordList[i][2]]
     return coordList
 
 
@@ -40,16 +35,21 @@ def coords3Dto2D_XYPlane(viewpoint, point):
         distZ = abs(viewpoint[2] - point[2])
         betweenX = point[0] * abs(viewpoint[2] / distZ) + viewpoint[0] * abs(point[2] / distZ)
         betweenY = point[1] * abs(viewpoint[2] / distZ) + viewpoint[1] * abs(point[2] / distZ)
-        print([betweenX, betweenY, 0])
         return [betweenX, betweenY]
     else:
         multiplier = point[2] / (viewpoint[2] - point[2])
         newX = point[0] + (point[0] - viewpoint[0]) * multiplier
         newY = point[1] + (point[1] - viewpoint[1]) * multiplier
-        print([newX, newY, 0])
         return [newX, newY]
 
 
+# list = PointList
+def updatePoints(list, viewpoint):
+    for i in range(len(list)):
+        list[i].coords2D = coords3Dto2D_XYPlane(viewpoint, list[i].coords)
+        list[i].distance = distance(viewPoint, list[i].coords)
+
+# The data structure
 
 class point:
     def __init__(self, coords, order):
@@ -61,14 +61,13 @@ class point:
 
 
 class face:
-    def __init__(self, points):
+    def __init__(self, points, color):
         self.hasBeenDrawn = False
         self.points = points
+        self.color = color
+
     def drawable(self):
         return all([i.drawable for i in self.points])
-    #Returns True if if all the points of this face are drawable
-    #and the face has not already been drawn
-    #once this returns True, status id changed to be drawn
 
     def point2DCoords(self):
         p = []
@@ -77,57 +76,47 @@ class face:
         return p
 
 
-
 class cube:
     def __init__(self, faces):
         self.faces = faces
 
+RGB_Palette = ((0, 155, 72), (255, 255, 255), (183, 18, 52), (255, 213, 0), (0, 70, 173), (255, 88, 0))
 
-
-loop = [[0,0],[0,1024],[1024,1024],[1024,0]]
-
-#lisätään kaikki 8 pistettä
-pointList = []
-for i in range(2):
-    for j in range(4):
-        pointList.append(point(loop[j] + [i*1024], j+4*i))
-        #print([loop[j] + [i*1024]])
-
-
-
-#ja sit viel 6 naamaa
-faceList = []
-for i in range(2):
-    faceList.append(face(pointList[(i*4):(i*4+4)]))
-
-for i in range(4):
-    faceList.append(face([pointList[0+i], pointList[(1+i)%4], pointList[(5+i)%8], pointList[4+i]]))
-
-for i in range(len(faceList)):
-    points = []
-    for j in range(4):
-        points.append(faceList[i].points[j].coords)
-    print(points)
-
-print("------")
+def createCube(center, size, viewpoint):
+    loop = [[center[0]-0.5*size, center[1]-0.5*size], [center[0]-0.5*size, center[1]+0.5*size], [center[0]+0.5*size, center[1]+0.5*size], [center[0]+0.5*size, center[1]-0.5*size]]
+    pointList = []
+    for i in range(2):
+        for j in range(4):
+            pointList.append(point(loop[j] + [center[2]+(i-0.5)*size], j + 4 * i))
+    faceList = []
+    for i in range(2):
+        faceList.append(face(pointList[(i * 4):(i * 4 + 4)], RGB_Palette[i]))
+    for i in range(4):
+        faceList.append(face([pointList[0 + i], pointList[(1 + i) % 4], pointList[(5 + i) % 8], pointList[4 + i]], RGB_Palette[i+2]))
+    updatePoints(pointList, viewpoint)
+    return pointList, faceList
 
 
 # later faces are added here farthest from nearest to the viewpoint
 drawQueue = []
 
-
 # prep image
 im = Image.open("BlankImage.png")
 draw = ImageDraw.Draw(im)
 
+viewPoint = [2048+1024, 2048+1024, 4096+1024]
+pointList, faceList = [],[]
 
+# creates the famous mushroom thing
+for i in range(3):
+    for j in range(3):
+        for k in range(3):
+            if not (j == k == 1 or i == j == 1 or i == k == 1):
 
-viewPoint = [2048, 2048, 4096]
+                tempP, tempL = createCube((i*1024-512, j*1024-512, k*1024-512), 1024, viewPoint)
+                pointList += tempP
+                faceList += tempL
 
-# set distance and 2D coordinate attributes
-for i in range(len(pointList)):
-    pointList[i].distance = distance(viewPoint, pointList[i].coords)
-    pointList[i].coords2D = coords3Dto2D_XYPlane(viewPoint, pointList[i].coords)
 
 # sort the points by distance
 tempOrder = pointList.copy()
@@ -142,24 +131,23 @@ for i in range(len(pointList)):
         if faceList[j].drawable() and not faceList[j].hasBeenDrawn:
             faceList[j].hasBeenDrawn = True
             newDrawables.append(j)
-    #calculates how far the points of each face are on average from the viewpoint, decides drawing order based on that
+    # calculates how far the points of each face are on average from the viewpoint, decides drawing order based on that
     newDrawables.sort(key=lambda x: sum([faceList[x].points[k].distance for k in range(4)]), reverse=True)
     drawQueue += newDrawables
-    #print(newDrawables)
-#print(drawQueue)
-#drawQueue tells in which order tha faces of facelist should be drawn
+
+zoom = 0.1
+shift = (128*3, 128*3)
+
 for i in range(len(drawQueue)):
-    #points = [faceList[drawQueue[i]].points[j].coords2D for j in range(4)]
     temp = faceList[drawQueue[i]].points
     points = [temp[k].coords2D for k in range(len(temp))]
-    n = []
-    n.append([temp[k].coords for k in range(len(temp))])
-    #print(n, points)
-    #print(points, temp)
+
     draw.polygon(
-        ((points[0][0] / 2 + 128 * 3, points[0][1] / 2 + 128 * 3),
-         (points[1][0] / 2 + 128 * 3, points[1][1] / 2 + 128 * 3),
-         (points[2][0] / 2 + 128 * 3, points[2][1] / 2 + 128 * 3),
-         (points[3][0] / 2 + 128 * 3, points[3][1] / 2 + 128 * 3)),
-        fill=(255, 20, 20),
+        ((points[0][0] * zoom + shift[0], points[0][1] * zoom + shift[1]),
+         (points[1][0] * zoom + shift[0], points[1][1] * zoom + shift[1]),
+         (points[2][0] * zoom + shift[0], points[2][1] * zoom + shift[1]),
+         (points[3][0] * zoom + shift[0], points[3][1] * zoom + shift[1])),
+        fill=(faceList[drawQueue[i]].color),
         outline=(0, 0, 0))
+
+im.show()
