3aaad7c497c0b4e2731afe9cd8196ace653b4229
valaisu <63551725+valaisu@users.noreply.github.com>
2023-02-09 20:21:46 +0200

    Update drawExperimanes.py
    Updated drawing logic so user can rotate the cube around. Math seems to only work when x, y >= 0

--- 3DRubicsCube/drawExperimanes.py
+++ 3DRubicsCube/drawExperimanes.py
@@ -159,6 +159,121 @@ def carthesian(r, a, b):
     z = r * math.cos(a / 180 * math.pi)
     return [x, y, z]
 
+# more advanced drawing functions
+
+def pointOntoPlane(point1, point2, plane):
+    # materials used:
+    # https://math.stackexchange.com/questions/1429131/find-an-equation-of-the-plane-perpendicular-to-vector-v-and-passing-through-the
+    # https://www.geeksforgeeks.org/equation-of-a-line-in-3d/
+
+    # returns where a line defined by 2 points intersects a plane in 3d space
+    # the plane always intersects the 0,0,0
+
+    # example:
+    # point1 = 1,2,3
+    # point2 = 4,5,6
+    # plane  = 1,1,2 <- later referred as px, py, pz
+
+    # (line orthogonal to plane equation: x = y = 1/2 z) not needed here
+    # plane equation: x + y + 2*z = 0
+    # 1, 2, 3 -> 4, 5, 6    the direction does not matter
+    # delta = 4-1, 5-2, 6-3 = 3,3,3
+
+    delta = [point1[0] - point2[0], point1[1] - point2[1], point1[2] - point2[2]]
+
+    # line equation: (x-1)/3 = (y-2)/3 = (z-3)/3
+    # line equation: (x-point1[0])/delta[0] = (y-point1[1])/delta[1] = (z-point1[2])/delta[2]
+
+    # solving y as x:
+    # (x-1)/3 = (y-2)/3
+    # (x - point1[0]) / delta[0] = (y - point1[1]) / delta[1]
+    # (x - point1[0]) / delta[0] * delta[1] = (y - point1[1])
+    # y = (x - point1[0]) / delta[0] * delta[1] + point1[1]
+    # =>
+    # y = x / delta[0] * delta[1] - point1[0] / delta[0] * delta[1] + point1[1]
+
+    # solving z as x:
+    # works the same way
+    # z = (x - point1[0]) / delta[0] * delta[2] + point1[2]
+    # z = x / delta[0] * delta[2] - point1[0] / delta[0] * delta[2] + point1[2]
+
+    # The whole equation
+    # solve x:
+    # the plane equation: px * x + py * y + pz * z = 0
+    # =>
+    # px * x + py * y + pz * z = 0
+    # px * x + py * x * delta[1] / delta[0] + pz * x * delta[2] / delta[0] =
+    # pz * (point1[0] / delta[0] * delta[2] - point1[2]) + py * (point1[0] / delta[0] * delta[1] - point1[1]) =>
+    # x * (px + py * delta[1] / delta[0]+ pz * delta[2] / delta[0]) =
+    # pz * (point1[0] / delta[0] * delta[2] - point1[2]) + py * (point1[0] / delta[0] * delta[1] - point1[1]) =>
+    # x = (pz * (point1[0] / delta[0] * delta[2] - point1[2]) + py * (point1[0] / delta[0] * delta[1] - point1[1])) / (px + py * delta[1] / delta[0]+ pz * delta[2] / delta[0])
+
+    # solving z as y:
+    # z = (y - point1[1]) / delta[1] * delta[2] + point1[2]
+    # z = y / delta[1] * delta[2] - point1[1] / delta[1] * delta[2] + point1[2]
+
+    # solve y:
+    # px * x + py * y + pz * z = 0
+    # pz * (y / delta[1] * delta[2]) + py * y = -px * x + pz * (point1[1] * delta[2] / delta[1] - point1[2])
+    # y * (py + pz * delta[2] / delta[1]) = -px * x + pz * (point1[1] * delta[2] / delta[1] - point1[2])
+    # y = (-px * x + pz * (point1[1] * delta[2] / delta[1] - point1[2])) / (py + pz * delta[2] / delta[1])
+
+    # solve z:
+    # px * x + py * y + pz * z = 0
+    # z = -(px * x + py * y) / pz
+
+    px, py, pz = plane[0], plane[1], plane[2]
+    try:
+        xCoord = (pz * (point1[0] / delta[0] * delta[2] - point1[2]) + py * (point1[0] / delta[0] * delta[1] - point1[1])) / (px + py * delta[1] / delta[0]+ pz * delta[2] / delta[0])
+        yCoord = (-px * xCoord + pz * (point1[1] * delta[2] / delta[1] - point1[2])) / (py + pz * delta[2] / delta[1])
+        zCoord = -(px * xCoord + py * yCoord) / pz
+        return [xCoord, yCoord, zCoord]
+    except ZeroDivisionError:
+        # this error should be rare, and when it occurs, nothing should be drawn anyways.
+        # I will deal with this properly (temporally) later.
+        return [0, 0, 0]
+
+
+def rotatePoints(vector, pointsRot):
+    # the idea is to rotate points on a plane defined by orthogonal vector so, that
+    # the vector points to dir 0,0,1.
+
+    # Material used:
+    # https://stackoverflow.com/questions/14607640/rotating-a-vector-in-3d-space
+    # https://matthew-brett.github.io/teaching/rotation_2d.html
+    dir = [0,0,1]
+
+    # angle between 2 vectors: cos(alpha) = a.b/(|a||b|)
+    # xz plane
+    dotProd = vector[0] * dir[0] + vector[2] * dir[2]
+    lenProd = math.sqrt(vector[0] ** 2 + vector[2] ** 2) * math.sqrt(dir[0] ** 2 + dir[2] ** 2)
+    beta = -math.acos(dotProd / lenProd)
+
+    # rotation around y-axis:
+    # | x cos θ + z sin θ| = |x'|
+    # |−x sin θ + z cos θ| = |z'|
+    for i in range(len(pointsRot)):
+        pointsRot[i] = [pointsRot[i][0] * math.cos(beta) + pointsRot[i][2] * math.sin(beta), pointsRot[i][1],
+                     -pointsRot[i][0] * math.sin(beta) + pointsRot[i][2] * math.cos(beta)]
+    newVector = [vector[0] * math.cos(beta) + vector[2] * math.sin(beta), vector[1],
+                 -vector[0] * math.sin(beta) + vector[2] * math.cos(beta)]
+
+    # yz plane
+    dotProd = newVector[1] * dir[1] + newVector[2] * dir[2]
+    lenProd = math.sqrt(newVector[1] ** 2 + newVector[2] ** 2) * math.sqrt(dir[1] ** 2 + dir[2] ** 2)
+    alpha = math.acos(dotProd / lenProd)
+
+    # rotation around x-axis:
+    # | y cos θ − z sin θ| = |y'|
+    # | y sin θ + z cos θ| = |z'|
+    for i in range(len(pointsRot)):
+        pointsRot[i] = [pointsRot[i][0], pointsRot[i][1] * math.cos(alpha) - pointsRot[i][2] * math.sin(alpha),
+                     pointsRot[i][1]*math.sin(alpha) + pointsRot[i][2]*math.cos(alpha)]
+
+    # finalVector = [newVector[0], newVector[1] * math.cos(alpha) - newVector[2] * math.sin(alpha),
+    #               newVector[1] * math.sin(alpha) + newVector[2] * math.cos(alpha)]
+
+    return pointsRot
 
 # CREATING THE DISPLAY
 
@@ -183,7 +298,7 @@ def poly(surf, color, coords):
     pygame.draw.polygon(surf, (0,0,0), coords, 2)
 
 moveInstructions = [0,0,0]
-
+clockCounter = 0
 a = 45
 b = 60
 alpha = 0
@@ -201,9 +316,9 @@ while not crashed:
             if event.key == pygame.K_UP:
                 alpha += 0.6
             if event.key == pygame.K_LEFT:
-                beta += 6
+                beta += 0.6
             if event.key == pygame.K_RIGHT:
-                beta -= 6
+                beta -= 0.6
 
             if event.key == pygame.K_q:
                 moveInstructions[2] += 100
@@ -216,18 +331,15 @@ while not crashed:
             if event.key == pygame.K_UP:
                 alpha -= 0.6
             if event.key == pygame.K_LEFT:
-                beta -= 6
+                beta -= 0.6
             if event.key == pygame.K_RIGHT:
-                beta += 6
+                beta += 0.6
 
             if event.key == pygame.K_q:
                 moveInstructions[2] -= 100
             if event.key == pygame.K_a:
                 moveInstructions[2] += 100
 
-    '''viewPoint[0] += moveInstructions[0]
-    viewPoint[1] += moveInstructions[1]
-    viewPoint[2] += moveInstructions[2]'''
     a += alpha
     b += beta
     viewPoint = carthesian(dist, a, b)
@@ -238,10 +350,13 @@ while not crashed:
     for i in range(len(faceList)):
         faceList[i].hasBeenDrawn = False
     updatePoints(pointList, viewPoint)
+
     drawQueue = calculateDrawOrder(pointList, faceList)
     for i in range(len(drawQueue)):
         temp = faceList[drawQueue[i]].points
-        points = [temp[k].coords2D for k in range(len(temp))]
+        points = [pointOntoPlane(viewPoint, temp[k].coords, viewPoint) for k in range(len(temp))]
+        points = rotatePoints(viewPoint, points)
+
         poly(gameDisplay, faceList[drawQueue[i]].color,
             ((points[0][0] * zoom + shift[0], points[0][1] * zoom + shift[1]),
              (points[1][0] * zoom + shift[0], points[1][1] * zoom + shift[1]),
@@ -249,6 +364,8 @@ while not crashed:
              (points[3][0] * zoom + shift[0], points[3][1] * zoom + shift[1])))
 
     pygame.display.update()
+    clockCounter += 1
+    if clockCounter % 30 == 0: print(carthesian(dist, a, b))
     clock.tick(60)
 
 pygame.quit()
