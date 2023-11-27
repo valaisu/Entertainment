0001aac5c53403539ffe252d3201db150e1610cf
valaisu <63551725+valaisu@users.noreply.github.com>
2023-11-27 14:01:40 +0200

    Add features
    Draw the terrain and units, add click detection and buttons

--- CiV simulator/rules.py
+++ CiV simulator/rules.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import math
 import numpy as np
+import random
 
 
 """
@@ -12,7 +13,7 @@ battle simulator for 2+ units.
 Roadmap:
 Create the board from hexagons CHECK
     draw the hexagons CHECK
-    add hills, rivers, forests
+    add hills, rivers, forests CHECK
 Create the units 
     unit movement
     unit attack
@@ -30,27 +31,102 @@ pygame.init()
 
 # Constants
 SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+DISPLAY_HEIGHT = 600
+SCREEN_HEIGHT = 600+100
 TILE_SIZE = 50  # Size of each hexagon tile
+CENTER = (SCREEN_WIDTH/2, DISPLAY_HEIGHT/2)
 
 # Colors
 WHITE = (255, 255, 255)
+GREEN = (50, 220, 50)
+BLUE_D = (100, 100, 200)
 BLACK = (0, 0, 0)
+YELLOW = (200,200,50)
 
 # positive directions are y: up, x, up-right
 # but as the hexagon has 6 sides, some re-defining needs to be made
 directions = [(0, 1), (1, 0), (1, -1), (0, -1), (-1, 0), (-1, 1)]
 
+# Terrain
+hills = pygame.transform.scale(pygame.image.load("hills.png"), (100, 100))
+forest = pygame.transform.scale(pygame.image.load("forest.png"), (100, 100))
+empty = pygame.transform.scale(pygame.image.load("empty.png"), (100, 100))
 
-class square:
-    def __init__(self, x, y, size, terrain):
+# Units
+warrior = pygame.transform.scale(pygame.image.load("warrior.png"), (60, 60))
+archer = pygame.transform.scale(pygame.image.load("archer.png"), (60, 60))
+
+# Buttons
+move = pygame.transform.scale(pygame.image.load("move.png"), (60, 60))
+target = pygame.transform.scale(pygame.image.load("target.png"), (60, 60))
+fortify = pygame.transform.scale(pygame.image.load("fortify.png"), (60, 60))
+
+terrains_dict = {0: empty, 1: hills, 2: forest}
+
+
+class Square:
+    def __init__(self, x, y, size, terrain, board_center):
         self.x = x
         self.y = y
         self.size = size
         self.terrain = terrain
+        # terrains: 0 = plains, 1 = hills, 2 = forest, 3 = forest and hills, 4 = water
+        self.unit = None
+
+        self.center = (np.cos(np.pi/6)*2*x*size*np.sin(np.pi/3), np.cos(np.pi/6)*(y + x*np.cos(np.pi/3))*2*size)
 
 
-def are_neighbors(s1, s2):
+# warrior: 2 movement, melee
+class Unit:
+    def __init__(self, type, location):
+        self.type = type
+        self.health = 100
+        self.attack = 20
+        self.movement_max = 2
+        self.movement_left = self.movement_max
+        self.location = location
+        self.image = warrior
+
+        self.fortified = False
+
+        self.ranged = False
+        self.range = 0
+
+
+def check_for_button_clicked(button_center, click_location, radius=30):
+    """
+    Checks if click is within [radius] pixels from the click location
+    :param button_center: (float, float)
+    :param click_location: (int, int)
+    :return: Bool
+    """
+    if math.sqrt(math.pow(button_center[0]-click_location[0], 2) + math.pow(button_center[1]-click_location[1], 2)) < radius:
+        return True
+    else:
+        return False
+
+
+def draw_action_bar(surface):
+    # TODO: add actions to the buttons
+    pygame.draw.polygon(surface, BLUE_D, [[0, 600], [800, 600], [800, 700], [0, 700]])
+    surface.blit(move, (560, 620))
+    surface.blit(target, (630, 620))
+    surface.blit(fortify, (700, 620))
+
+
+def move_unit(unit, sq_from, sq_to):
+    """
+    Moves a unit from one square to another
+    :param unit:
+    :param sq_from:
+    :param sq_to:
+    :return: None
+    """
+    sq_from.unit = None
+    sq_to.unit = unit
+
+
+def are_neighbors(s1: Square, s2: Square):
     """
     Checks, weather squares are adjacent to each other
     :param s1: square
@@ -58,6 +134,14 @@ def are_neighbors(s1, s2):
     :return: Bool
     """
     return (s1.x-s2.x, s1.y-s2.y) in directions
+
+def get_neighbors(s: Square):
+    """
+    Gets the neighbors of a square
+    :param s: square
+    :return: list[square]
+    """
+    return [(s.x+d[0], s.y+d[1]) for d in directions]
 
 
 def create_board(amount: int, size: float):
@@ -67,7 +151,7 @@ def create_board(amount: int, size: float):
     :param size:
     :return: list[square]
     """
-    hexes = [square(0,0,50, 0)]
+    hexes = [Square(0,0,50, 1, CENTER)]
     tot = 0
     for i in range(amount):
         for dir in range(len(directions)):
@@ -75,34 +159,74 @@ def create_board(amount: int, size: float):
                 tot += 1
                 x = (i+1)*directions[dir][0] + j*directions[(dir-2)%6][0]
                 y = (i+1)*directions[dir][1] + j*directions[(dir-2)%6][1]
-                hexes.append(square(x, y, size, 0))
+                ter = random.choices([0, 1, 2], weights=[2, 1, 1])[0]
+                hexes.append(Square(x, y, size, ter, CENTER))
     return hexes
 
 
 # Function to draw a hexagon
-def draw_hexagon(surface, x: int, y: int, size: float, center: (int, int)):
+def draw_hexagon(surface, sq: Square, center: (int, int)):
     """
     Draws a single hexagon using information from the square-class and
     information about the surface
     :param surface:
-    :param x: int
-    :param y: int
-    :param size: float
+    :param sq: square
     :param center: (int, int)
     :return: None
     """
-    base_location = (np.cos(np.pi/6)*2*x*size*np.sin(np.pi/3), np.cos(np.pi/6)*(y + x*np.cos(np.pi/3))*2*size)
+    x = sq.x
+    y = sq.y
+    size = sq.size
+    type = sq.terrain
     angle = np.pi/3
     points = []
     for i in range(6):
-        x_i = x + size * math.cos(angle * i) + base_location[0] + center[0]
-        y_i = y + size * math.sin(angle * i) + base_location[1] + center[1]
+        x_i = x + size * math.cos(angle * i) + sq.center[0] + center[0]
+        y_i = y + size * math.sin(angle * i) + sq.center[1] + center[1]
         points.append((x_i, y_i))
+    pygame.draw.polygon(surface, GREEN, points)
     pygame.draw.polygon(surface, WHITE, points, 2)
 
+    surface.blit(terrains_dict[type], (sq.center[0]+center[0]-50, sq.center[1]+center[1]-50))
+    if sq.unit:
+        surface.blit(sq.unit.image, (sq.center[0]+center[0]-30, sq.center[1]+center[1]-30))
+
+
+def highlight_hexagon(surface, sq: Square, center: (int, int)):
+    """
+    Highlights a single hexagon
+    :param surface:
+    :param sq: Square
+    :param center: (int, int)
+    :return: None
+    """
+    x = sq.x
+    y = sq.y
+    size = sq.size
+    angle = np.pi/3
+    points = []
+    for i in range(6):
+        x_i = x + size * math.cos(angle * i) + sq.center[0] + center[0]
+        y_i = y + size * math.sin(angle * i) + sq.center[1] + center[1]
+        points.append((x_i, y_i))
+    pygame.draw.polygon(surface, YELLOW, points, 6)
+
+
+def new_turn(board: list[Square]):
+    """
+    Heals units and refreshes their movement
+    :param board:
+    :return:
+    """
+    for sq in board:
+        if not sq.unit:
+            continue
+        if sq.unit.movement_left == sq.unit.movement_max:
+            sq.unit.health = min(100, sq.unit.health+10)
+        sq.unit.movement_left = sq.unit.movement_max
 
 # Function to draw the hexagon board
-def draw_hexagon_board(surface, squares: list[square]):
+def draw_hexagon_board(surface, squares: list[Square]):
     """
     Loops through the hexagons and individually draws them
     :param surface:
@@ -110,7 +234,25 @@ def draw_hexagon_board(surface, squares: list[square]):
     :return: None
     """
     for sq in squares:
-        draw_hexagon(surface, sq.x, sq.y, sq.size, (400, 300))
+        draw_hexagon(surface, sq, (400, 300))
+
+
+def click_square(x: float, y: float, center: (int, int), hexes: list[Square]):
+    """
+    returns the square clicked
+    :param x:
+    :param y:
+    :param center:
+    :param list[Square]:
+    :return: Square
+    """
+    distances = [(np.sqrt(math.pow(hexes[i].center[0]+center[0]-x, 2) + math.pow(hexes[i].center[1]+center[1]-y, 2)),i) for i in range(len(hexes))]
+    distances.sort()
+    if distances[0][0] < 60:
+        return distances[0][1]
+    else:
+        return None
+
 
 
 hexagons = create_board(2, 50)
@@ -118,20 +260,35 @@ hexagons = create_board(2, 50)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Hexagon Board')
 
+
+# MANUALLY ADD UNITS HERE
+hexagons[9].unit = Unit(1, (hexagons[9].x, hexagons[9].y))
+hexagons[15].unit = Unit(1, (hexagons[15].x, hexagons[15].y))
+
+selected_hex = None
+
 # Main game loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos
+            selected_hex = (click_square(x, y, (400, 300), hexagons))
+            print(selected_hex)
 
     screen.fill(BLACK)
 
     # Draw the hexagon board (5x5 in this example)
     draw_hexagon_board(screen, hexagons)
 
+    draw_action_bar(screen)
+
+    # Highlights a selected hexagon
+    if selected_hex is not None:
+        highlight_hexagon(screen, hexagons[selected_hex], CENTER)
+
     pygame.display.flip()
-
-
 
 
