b82ba2ae49955c5c9f108466a93c6d58df028cc9
valaisu <63551725+valaisu@users.noreply.github.com>
2023-11-28 20:45:13 +0200

    add team and combat

--- CiV simulator/rules.py
+++ CiV simulator/rules.py
@@ -73,6 +73,22 @@ empty = pygame.transform.scale(pygame.image.load("empty.png"), (100, 100))
 warrior = pygame.transform.scale(pygame.image.load("warrior.png"), (60, 60))
 archer = pygame.transform.scale(pygame.image.load("archer.png"), (60, 60))
 
+
+def change_color(image_path: str, color: (int, int, int, int)):
+    img = pygame.transform.scale(pygame.image.load(image_path), (60, 60))
+    for x in range(img.get_width()):
+        for y in range(img.get_height()):
+            pixel_color = img.get_at((x, y))
+            if pixel_color == (0, 0, 0, 255):  # Check if the pixel is black
+                img.set_at((x, y), color)  # Change to blue
+    return img
+
+
+warrior_blue = change_color("warrior.png", (100, 100, 200, 255))
+warrior_red = change_color("warrior.png", (200, 50, 50, 255))
+warrior_list = [warrior_blue, warrior_red]
+# TODO: make above process a function
+
 # Action bar buttons
 move = pygame.transform.scale(pygame.image.load("move.png"), (60, 60))
 target = pygame.transform.scale(pygame.image.load("target.png"), (60, 60))
@@ -109,19 +125,21 @@ class Square:
 
 # warrior: 2 movement, melee
 class Unit:
-    def __init__(self, type, location):
+    def __init__(self, type, location, team, image):
         self.type = type
         self.health = 100
-        self.attack = 20
+        self.strength = 20
         self.movement_max = 2
         self.movement_left = self.movement_max
         self.location = location
-        self.image = warrior
+        self.image = warrior_list[team-1]
 
         self.fortified = False
 
         self.ranged = False
         self.range = 0
+
+        self.team = team
 
 
 def check_for_button_clicked(button: Button, click_location):
@@ -346,6 +364,29 @@ def click_square(x: float, y: float, center: (int, int), hexes: list[Square]):
         return None
 
 
+def combat_melee(attacker: Square, defender: Square):
+    attacker.unit.movement_left = 0
+    strength_difference = attacker.unit.strength - defender.unit.strength
+    attacker.unit.health = round(attacker.unit.health-30*np.exp(-strength_difference/25))
+    defender.unit.health = round(defender.unit.health-30*np.exp(strength_difference/25))
+    # if both "die", only the one that dies more really dies
+    if attacker.unit.health <= 0 and defender.unit.health <= 0:
+        if attacker.unit.health > defender.unit.health:
+            attacker.unit.health = 1
+            defender.unit.health = 0
+        else:
+            attacker.unit.health = 0
+            defender.unit.health = 1
+    # if attacker dies
+    if attacker.unit.health <= 0:
+        attacker.unit = None
+    # if defender dies
+    if defender.unit.health <= 0:
+        defender.unit = attacker.unit
+        defender.unit.movement_left = 0
+        attacker.unit = None
+
+
 def unselect_buttons(buttons: list[Button]):
     for button in buttons:
         button.selected = False
@@ -366,7 +407,8 @@ def display_stats(surface, unit: Unit):
 
     draw_text(str(unit.health), 18, 200, 620)
     draw_text(str(unit.movement_left) + "/" + str(unit.movement_max), 18, 200, 645)
-    draw_text(str(unit.attack), 18, 200, 670)
+    draw_text(str(unit.strength), 18, 200, 670)
+
 
 hexagons = create_board(2, 50)
 # Create the screen
@@ -375,8 +417,8 @@ pygame.display.set_caption('Hexagon Board')
 
 
 # MANUALLY ADD UNITS HERE
-hexagons[9].unit = Unit(1, (hexagons[9].x, hexagons[9].y))
-hexagons[15].unit = Unit(1, (hexagons[15].x, hexagons[15].y))
+hexagons[9].unit = Unit(1, (hexagons[9].x, hexagons[9].y), 1, warrior_blue)
+hexagons[15].unit = Unit(1, (hexagons[15].x, hexagons[15].y), 2, warrior_red)
 
 selected_hex = None
 
@@ -403,10 +445,16 @@ while True:
                 if selected_hex is not None and new_selection is not None:
                     if button_list[0].selected:
                         # check if enough movement
+                        # TODO: if full movement, can always move one
                         movement_required = pathfinding(hexagons[selected_hex], hexagons[new_selection], hexagons)
                         if hexagons[selected_hex].unit.movement_left >= movement_required:
-                            hexagons[selected_hex].unit.movement_left -= movement_required
-                            move_unit(hexagons[selected_hex].unit, hexagons[selected_hex], hexagons[new_selection])
+                            # check if initiates combat
+                            if hexagons[new_selection].unit:
+                                if hexagons[selected_hex].unit.team != hexagons[new_selection].unit.team:
+                                    combat_melee(hexagons[selected_hex], hexagons[new_selection])
+                            else:
+                                hexagons[selected_hex].unit.movement_left -= movement_required
+                                move_unit(hexagons[selected_hex].unit, hexagons[selected_hex], hexagons[new_selection])
                         else:
                             print("not enough movement")
 
