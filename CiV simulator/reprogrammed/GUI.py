b4c025be167fd24a07fbbeee22876462adaaf6de
valaisu <63551725+valaisu@users.noreply.github.com>
2023-12-06 17:08:51 +0200

    Create GUI.py

---
+++ CiV simulator/reprogrammed/GUI.py
@@ -0,0 +1,34 @@
+from gameLogic import Game, Turns, Square, Teams
+from gameLogic import movement_costs, terrain_combat_modifier
+from units import Unit
+
+
+"""
+
+Draw squares
+
+Draw units
+
+Draw action bar
+    buttons
+
+
+"""
+
+
+def draw_board(game: Game):
+
+    def draw_square(sq: Square):
+        # draw terrain
+        # draw unit
+        pass
+
+    for square in game.squares:
+        draw_square(square)
+
+
+def draw_action_bar():
+    # draw buttons
+    pass
+
+
